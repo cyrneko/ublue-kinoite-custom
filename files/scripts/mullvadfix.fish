3fdc2422397584d1ad4e476e38f8e8d9eedb831d
alexia <cyrus42@disroot.org>
2025-07-20 17:05:37 +0200

    haha fune

--- files/scripts/mullvadfix.fish
+++ files/scripts/mullvadfix.fish
@@ -2,9 +2,12 @@
 
 mkdir -p /tmp/mullvad_extract && cd /tmp/mullvad_extract
 
-wget "https://mullvad.net/en/download/app/rpm/latest" --output-document mullvad.rpm
+wget --quiet "https://mullvad.net/en/download/app/rpm/latest" --output-document mullvad.rpm
 
+ls -la
+echo "rpm2archive NOW"
 rpm2archive mullvad.rpm && tar xvf mullvad.rpm.tgz
+ls -la
 
 cp -r opt/Mullvad\ VPN/ /opt/
 
